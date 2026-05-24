f4bf86f4d395a17bbc8fe1093a193bd4f06759a6
ManojV <manojvrajan9@gmail.com>
2026-05-24 15:39:36 +0800

    Add files via upload

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -24,6 +24,15 @@ namespace LearnerDash
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
 
         /// <summary>
+        /// NotificationBell1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UserControl NotificationBell1;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
